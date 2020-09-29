a57612ee9b35add87e5321768b9c82123fb4016c
Oleksandra <karska.dev@gmail.com>
2020-09-28 23:06:48 -0700

    Setup structure. Add SCSS. Start to craft Header.

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Attribution from "./Attribution";
+
+import "../scss/app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Attribution />
+    
+      </div>
+  );
+}
+
+export default App;
